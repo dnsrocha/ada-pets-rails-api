56288fcd3682880757a53f19392f6d46a51d50ca
dnsrocha <drocha@gmail.com>
2020-12-02 10:24:26 -0800

    test update

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,6 +1,7 @@
 class PetsController < ApplicationController
   def index
-    @pets = Pet.all.order(:name)
-    render json: {ok: 'YUP'}, status: :ok
+    pets = Pet.all.order(:name)
+    render json: pets.as_json(only: [:id, :name, :age, :owner, :species]),
+           status: :ok
   end
 end
--- test/controllers/pets_controller_test.rb
+++ test/controllers/pets_controller_test.rb
@@ -7,4 +7,33 @@ describe PetsController do
     expect(response.header['Content-Type']).must_include 'json'
   end
 
+  it "will return all proper fields for a list of pets" do
+    pet_fields = ["id", "name", "age", "owner", "species"].sort
+
+    get pets_path
+
+    #get the body of the response as an array or hash
+    body = JSON.parse(response.body)
+
+    #assert
+    expect(body).must_be_instance_of Array
+
+    body.each do |pet|
+      expect(pet).must_be_instance_of Hash
+      expect(pet.keys.sort). must_equal pet_fields
+    end
+  end
+
+  it "returns empty array if no pets exist" do
+    Pet.destroy_all
+
+    get pets_path
+
+    #get the body of the response as an array or hash
+    body = JSON.parse(response.body)
+
+    expect(body).must_be_instance_of Array
+    expect(body.length).must_equal 0
+
+  end
 end
